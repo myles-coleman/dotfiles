f63b2ebaed4ec9a5afd2650d1adafbef75752914
Myles Coleman <mylescoleman05@gmail.com>
2025-08-17 01:16:56 -0700

    fixed touchscreen orientation

--- hypr/.config/hypr/start.sh
+++ hypr/.config/hypr/start.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Initialize wallpaper
 swww init &
-
 swww-daemon & sleep 0.1 && swww img ~/Wallpapers/wallpaper1.jpg &
 
+# Start network manager applet
 nm-applet --indicator &
 
+# Start waybar
 waybar &
 
+# Start notification daemon
 dunst
